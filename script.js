7baa57a24926b7534842ad81ff5e4773c67ee853
kaby <constantinbarbulat@gmail.com>
2023-03-18 23:59:07 +0100

    Finish

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const numberOfFilms = +prompt("Quanti film hai visto?");
 
+let personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false,
+
+};
+
+let title_film = prompt("Qual è il titolo dell'ultimo film che hai visto?");
+let rating_film = +prompt("Da' un voto all'ultimo film che hai visto");
+
+personalMovieDB.movies[title_film] = rating_film;
+
+
+
+
+
+
+console.log(personalMovieDB);
